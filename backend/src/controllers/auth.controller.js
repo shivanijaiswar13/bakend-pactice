5a76ff72f1d4a1b293484b2a72830c569e765e16
shivanijaiswar13 <shivanijaiswar01@gmail.com>
2026-07-21 17:13:12 +0530

    create get-me controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-const { response } = require("../app");
+
 const userModel = require("../models/user.model");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
@@ -88,6 +88,12 @@ async function loginUSerController(req,res){
     })
 }
 
+/**
+ * @name logoutUserController
+ * @description clear token from user cookie and add the token in blacklist
+ * @access public
+ */
+
 async function logoutUserController(req,res){
     const token = req.cookies.token
 
@@ -101,6 +107,25 @@ async function logoutUserController(req,res){
     })
 }
 
+/**
+ * @name getMeController
+ * @description get the current logged in user details.
+ * @access private
+ */
+
+async function getMeController(req,res) {
+    const user = await userModel.findById(req.user.id)
+    res.status(200).json({
+        
+        user:{
+            message: "user detailed fetch successfully",
+            id: user._id,
+            username: user.username,
+            email:user.email
+        }
+    })
+}
+
 module.exports ={
-    registerUserController,loginUSerController,logoutUserController
+    registerUserController,loginUSerController,logoutUserController,getMeController
 }
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const {Router} = require("express");
 const authController = require("../controllers/auth.controller")
+const authMiddleware = require("../middlewares/auth.middleware")
 
 const authRouter = Router()
 
@@ -23,5 +24,13 @@ authRouter.post("/login",authController.loginUSerController)
  * @access Public
  */
 authRouter.get("/logout",authController.logoutUserController)
+
+/**
+ * @route GET /api/auth/get-me
+ * @description get the current logged in user details
+ * @access private
+ */
+
+authRouter.get("/get-me",authMiddleware.authUser,authController.getMeController)
 
 module.exports = authRouter
