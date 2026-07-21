b002e0316f9258c646e04bd0dddc064122ab10db
shivanijaiswar13 <shivanijaiswar01@gmail.com>
2026-07-21 17:29:55 +0530

    token blacklisted setup

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,12 +1,22 @@
 const jwt = require("jsonwebtoken");
+const tokenBlackListModel = require("../models/blacklist.model")
 
 
-function authUser(req,res,next){
+async function authUser(req,res,next){
     const token = req.cookies.token
 
     if(!token){
         return res.status(401).json({
             message: "Token not provided"
+        })
+    }
+    const isTokenBlacklisted = await tokenBlackListModel.findOne({
+        token
+    })
+
+    if(!isTokenBlacklisted){
+        return res.status(401).json({
+            message:"Token is invalid.Please login again."
         })
     }
     try{
