5904526b424e33bd1d0f5a5175ba43de4a93d204
shivanijaiswar13 <shivanijaiswar01@gmail.com>
2026-07-22 17:55:37 +0530

    login page structure

--- Fronted/src/auth/pages/Login.jsx
+++ Fronted/src/auth/pages/Login.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import "../auth.form.scss"
 
 const Login = () => {
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+
+    }
   return (
-    <div>Login</div>
+    <main>
+        <div className="form-container">
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="input-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="email" placeholder='Enter email address' />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="password" placeholder='Enter password' />
+                </div>
+                <button className='button primary-button'>Login</button>
+            </form>
+        </div>
+    </main>
   )
 }
 
