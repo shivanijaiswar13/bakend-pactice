2e24f74e61bcfb7c4a71ef2b3b6bb6d383a23c24
shivanijaiswar13 <shivanijaiswar01@gmail.com>
2026-07-22 20:53:51 +0530

    registration form structure

--- Fronted/src/auth/pages/Register.jsx
+++ Fronted/src/auth/pages/Register.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 
-const register = () => {
+const Register = () => {
+  const handleSubmit = (e)=>{
+        e.preventDefault()
+
+    }
+
   return (
-    <div>register</div>
+    <main>
+        <div className="form-container">
+            <h1>Register</h1>
+            <form onSubmit={handleSubmit}>
+              <div className="input-group">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" name="username" id="username" placeholder='Enter username' />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="email" placeholder='Enter email address' />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="password" placeholder='Enter password' />
+                </div>
+                <button className='button primary-button'>Login</button>
+            </form>
+        </div>
+    </main>
   )
 }
 
-export default register
+export default Register
