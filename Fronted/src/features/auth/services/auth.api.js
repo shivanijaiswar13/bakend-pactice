bc31a4a35500652337b500c35e565dbdf0343253
shivanijaiswar13 <shivanijaiswar01@gmail.com>
2026-07-24 21:09:27 +0530

    registration function with api

---
+++ Fronted/src/features/auth/services/auth.api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+
+export async function register(username,email,password) {
+    try {
+        
+   
+    const response = await axios.post("http://localhost:3000/api/auth/register",{
+        username,email,password
+    },{
+        withCredentials:true
+    })
+    return response.data
+     } catch (error) {
+        console.log(error);
+    }
+}
