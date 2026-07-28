110e5ab8a044fe9746855d6ca2eebf70f31103d9
shivanijaiswar13 <shivanijaiswar01@gmail.com>
2026-07-28 20:33:43 +0530

    Auth.Context file completed

--- Fronted/src/features/auth/auth.context.jsx
+++ Fronted/src/features/auth/auth.context.jsx
@@ -1,5 +1,14 @@
-import { Children, createContext } from "react";
+import { Children, createContext ,useState} from "react";
 
 export const AuthContext = createContext()
 
-export const AuthProvider = ({Children}) => {}
+export const AuthProvider = ({Children}) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    return(
+        <AuthContext.Provider value = {{user,setUser,loading,setLoading}}>
+        {Children}
+        </AuthContext.Provider>
+    )
+}
