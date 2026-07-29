c2874780e5bffe60b06a8d75f011e7d5956beda4
shivanijaiswar13 <shivanijaiswar01@gmail.com>
2026-07-29 18:15:45 +0530

    error handling

--- Fronted/src/features/auth/hooks/useAuth.js
+++ Fronted/src/features/auth/hooks/useAuth.js
@@ -10,23 +10,43 @@ export const useAuth = () =>{
 
     const handleLogin = async ({email,password})=>{
         setLoading(true)
-        const data = await login({email,password})
+        try {
+            const data = await login({email,password})
 
         setUser(data.user)
-        setLoading(false)
+        } catch (error) {
+            console.log(error)
+        }finally{
+            setLoading(false)
+        }
+        
+        
 
     }
     const handleRegister = async ({username,email,password})=>{
         setLoading(true)
-        const data = await register({username,email,password})
+        try {
+            const data = await register({username,email,password})
         setUser(data.user)
-        setLoading(false)
+        } catch (error) {
+            console.log(error)
+        }finally{
+            setLoading(false)
+        }
+        
+        
     }
     const handleLogout = async ()=>{
         setLoading(true)
-        const data = await logout()
+        try {
+            const data = await logout()
         setUser(null)
-        setLoading(false)
+        } catch (error) {
+            console.log(error)
+        }finally{
+            setLoading(false)
+        }
+        
     }
     return {user,loading,handleRegister,handleLogin,handleLogout}
 }
--- Fronted/src/features/auth/pages/Login.jsx
+++ Fronted/src/features/auth/pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
     
     const handleSubmit = async (e)=>{
         e.preventDefault()
-        handleLogin({email,password})
+        await handleLogin({email,password})
 
     }
     if(loading){
