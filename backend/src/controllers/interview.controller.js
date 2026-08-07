c73982dac7f145f5530f4a5655679f6ae012c473
shivanijaiswar13 <shivanijaiswar01@gmail.com>
2026-08-07 16:09:08 +0530

    create interview report by Ai

--- backend/src/controllers/interview.controller.js
+++ backend/src/controllers/interview.controller.js
@@ -1,10 +1,17 @@
 const pdfParse = require("pdf-parse");
+const generateInterviewReport = require("../services/ai.service")
 
 async function generateInterviewReportController(req,res){
     const resumeFile = req.file 
     
     const resumeContent = pdfParse(req.file.buffer)
-    const {selfDescription,jobDescription} = req.body
+    const {selfDescription,jobDescription} = req.body;
+
+    const interviewReportByAi = await generateInterviewReport({
+        resume: resumeContent,
+        selfDescription,
+        jobDescription
+    })
 
 }
 
