dca1de38189470c62514a4f620aab713df426e8c
shivanijaiswar13 <shivanijaiswar01@gmail.com>
2026-08-07 16:24:55 +0530

    interview controller done

--- backend/src/controllers/interview.controller.js
+++ backend/src/controllers/interview.controller.js
@@ -1,5 +1,6 @@
 const pdfParse = require("pdf-parse");
 const generateInterviewReport = require("../services/ai.service")
+const interviewReportModel = require("../models/interviewReport.model")
 
 async function generateInterviewReportController(req,res){
     const resumeFile = req.file 
@@ -11,6 +12,18 @@ async function generateInterviewReportController(req,res){
         resume: resumeContent,
         selfDescription,
         jobDescription
+    })
+    const interviewReport = await interviewReportModel.create({
+        user: req.user.id,
+        resume:resumeContent,
+        selfDescription,
+        jobDescription,
+        ...interviewReportByAi
+    })
+
+    res.status(201).json({
+        message: "Interview report generated successfully",
+        interviewReport
     })
 
 }
